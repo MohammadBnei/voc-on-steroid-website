9819483bc1894012c2d67ff809f001bed1fd85df
mohammadbnei <mohammadamine.banaei@gmail.com>
2021-12-15 12:44:50 +0100

    Programmatically added prefetch because the shortcut is not working

--- src/lib/shared/components/word/ClickableWord.svelte
+++ src/lib/shared/components/word/ClickableWord.svelte
@@ -3,12 +3,15 @@
 </script>
 
 <script lang="ts">
+	import { prefetch } from '$app/navigation';
+
 	export let text: string;
 	export let href: string;
+	$: path = `/${href}`;
 </script>
 
 {#if text.length > 2}
-	<a sveltekit:prefetch href="{`/${href}`}" class="hover:font-bold">{text}</a>
+	<a on:mouseenter="{() => prefetch(path)}" href="{path}" class="hover:font-bold">{text}</a>
 {:else}
 	{text}
 {/if}
