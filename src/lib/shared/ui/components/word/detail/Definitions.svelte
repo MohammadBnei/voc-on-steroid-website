359d0f72ae00d78c38cdaa05cd8666f4c08d07d6
mohammadbnei <mohammadamine.banaei@gmail.com>
2022-01-12 04:15:42 +0100

    removed gray color on definition text

--- src/lib/shared/ui/components/word/detail/Definitions.svelte
+++ src/lib/shared/ui/components/word/detail/Definitions.svelte
@@ -13,11 +13,11 @@
 			{results.type}
 		</p>
 	</div>
-	<div class="p-5 pb-0 pt-3 text-gray-700 body-2">
+	<div class="p-5 pb-0 pt-3 body-2">
 		{#each results.definitions as { definition, examples }}
 			<div class="m-2">
 				<p class="font-medium"><ClickableText text="{definition}" /></p>
-				<p class="text-gray-700 text-base font-extralight antialiased"
+				<p class="text-base font-extralight antialiased"
 					><ClickableText text="{examples.join('\n')}" /></p
 				>
 			</div>
