ce6444e150a8097a3e90c24ba240ebe639a79bd4
mohammadbnei <mohammadamine.banaei@gmail.com>
2022-01-12 02:27:07 +0100

    resolved search bug

--- src/routes/endpoint/resemblance/index.ts
+++ src/routes/endpoint/resemblance/index.ts
@@ -1,8 +1,8 @@
 import * as api from '$lib/utils/api';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const get: RequestHandler = async ({ params, locals }) => {
-	const res = await api.get({ path: `${locals.WORD_API}resemblance?${params.query.toString()}` });
+export const get: RequestHandler = async ({ url, locals }) => {
+	const res = await api.get({ path: `${locals.WORD_API}resemblance${url.search}` });
 	const data = await api.handleRes(res, 'Endpoit:resemblance');
 
 	return {
