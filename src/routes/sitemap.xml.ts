8f8f98fbd05ab55d37923870d901916983bc4bc1
mohammadbnei <mohammadamine.banaei@gmail.com>
2022-01-13 14:27:04 +0100

    forgot these console logs

--- src/routes/sitemap.xml.ts
+++ src/routes/sitemap.xml.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 const URL = process.env.BASE_URL;
 const BASE_URL = URL ? URL : 'https://voconsteroid.com';
-const pages = [ '', 'about', 'account', 'assoc', 'endpoint', 'experimental' ];
+const pages = ['', 'about', 'account', 'assoc', 'endpoint', 'experimental'];
 
 const render = (pages: string[]) => `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
@@ -23,11 +23,9 @@ ${pages
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function get() {
-	console.log({ pages, BASE_URL, URL })
 	const sitemap = render(pages);
 
 	return {
 		body: sitemap,
-		log: { pages, BASE_URL, URL }
 	};
 }
