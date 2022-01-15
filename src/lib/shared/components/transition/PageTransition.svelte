8127b0dcc915701a63850ff5bf2090244e9c7b40
mohammadbnei <mohammadamine.banaei@gmail.com>
2022-01-15 20:15:29 +0100

    simple fade transition

--- src/lib/shared/components/transition/PageTransition.svelte
+++ src/lib/shared/components/transition/PageTransition.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { fade } from 'svelte/transition';
+	import { fade, crossfade } from 'svelte/transition';
 	export let refresh = '';
 </script>
 
 {#key refresh}
-	<div in:fade>
+	<div transition:fade>
 		<slot />
 	</div>
 {/key}
