bb1309dc413c9258f79db3ae1701cfb18cbf6ce9
mohammadbnei <mohammadamine.banaei@gmail.com>
2022-02-01 00:43:41 +0100

    animating saved word list

--- src/lib/shared/components/assoc/List.svelte
+++ src/lib/shared/components/assoc/List.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import type { AssocWord } from '$entity/assoc/services';
 	import Assoc from '$lib/shared/ui/components/word/Assoc.svelte';
+	import { fade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 
 	export let words: AssocWord[];
 
 	$: sortedWords = words.sort((a, b) => a?.id?.localeCompare(b.id));
 </script>
 
-{#each sortedWords as word}
-	<Assoc {...word} />
+{#each sortedWords as word (word.id)}
+	<div in:fade animate:flip>
+		<Assoc {...word} />
+	</div>
 {:else}
 	<div> No word yet. Go search for some </div>
 {/each}
