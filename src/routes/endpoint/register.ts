c8a594b6d53064cef3ce9f6da61b5f43d2158c3e
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-03-21 15:37:36 +0100

    Added direct login on register

--- src/routes/endpoint/register.ts
+++ src/routes/endpoint/register.ts
@@ -1,10 +1,13 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import * as api from '$lib/utils/api';
+import { getAuthCookies } from '$lib/utils/auth';
 
 export const post: RequestHandler = async ({ request, locals }) => {
 	const res = await api.post({
-		path: locals.USER_API + 'register', data: await request.json()
+		path: locals.USER_API + 'register',
+		data: await request.json(),
 	});
+
 	const data = await res.json();
 	if (!res.ok) {
 		return {
@@ -12,8 +15,9 @@ export const post: RequestHandler = async ({ request, locals }) => {
 			body: data,
 		};
 	}
-
+	const cookies = getAuthCookies({ user: data.user });
 	return {
+		...api.handleResponseCookies(res, ...cookies),
 		body: data,
 	};
 };
