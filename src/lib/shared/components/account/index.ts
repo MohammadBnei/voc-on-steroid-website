3e06cba9a2fd5acb9d6b718b9dd28f89f4bff1b4
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-04-29 11:07:05 +0200

    fixup

--- src/lib/shared/components/account/index.ts
+++ src/lib/shared/components/account/index.ts
@@ -1,5 +1,4 @@
 export { default as Login } from './Login.svelte';
-export { default as AuthHeader } from './AuthHeader.svelte';
 export { default as Profile } from './Profile.svelte';
 export { default as Register } from './Register.svelte';
 
