dc302ca0a4c7e2aaa26b6a63646b5d5ec7b9a23d
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-05-01 22:27:20 +0200

    updated cache value

--- src/stores.ts
+++ src/stores.ts
@@ -29,14 +29,12 @@ export const currentWord = writable<string | null>(null);
 const createAssocStore = () => {
 	const { subscribe, update, set } = writable<AssocWord[]>([]);
 
-	const updateList = (words: AssocWord[]) => update((list) => [...words, ...list]);
 	const addWord = (word: AssocWord) => update((list) => [word, ...list]);
 	const removeWord = (word: string) => update((list) => list.filter((v) => v.id !== word));
 	const setList = (words: AssocWord[]) => set([...words]);
 
 	return {
 		subscribe,
-		updateList,
 		addWord,
 		removeWord,
 		setList,
