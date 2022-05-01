6f0e172af632afe0e05df0c74cdfd0300ffbb648
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-05-01 04:46:44 +0200

    trying logout stuff

--- src/routes/endpoint/logout.ts
+++ src/routes/endpoint/logout.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 import { deleteCookies } from '$lib/utils/auth';
 
 export const get: RequestHandler = async ({ locals }) => {
-	await locals.fetch.get({ path: locals.USER_API + 'logout' });
+	locals.fetch.get({ path: locals.USER_API + 'logout' });
 
 	return {
 		status: 200,
