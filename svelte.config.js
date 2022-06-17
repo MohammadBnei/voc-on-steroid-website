6edc87218ddba23386dd881890b210ce1fbcd2f8
MohammadBnei <mohammadamine.banaei@gmail.com>
2022-06-18 00:47:51 +0200

    annoying

--- svelte.config.js
+++ svelte.config.js
@@ -50,13 +50,6 @@ const config = {
 		}),
 	],
 	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
-		amp: isAMP,
-
-		// hydrate the <div id="svelte"> element in src/app.html
-
 		vite: () => ({
 			ssr: {
 				noExternal: [...Object.keys(pkg.dependencies || {})],
